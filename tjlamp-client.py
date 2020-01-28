117e5b446269720300ad393da1d5088c9cafc322
Justin Weisz <jweisz@us.ibm.com>
2020-01-28 15:26:59 -0500

    tabs to spaces

--- tjlamp-client.py
+++ tjlamp-client.py
@@ -86,26 +86,26 @@ async def listen():
     uri = "ws://tjlamp.mybluemix.net:80/lamp"
     async with websockets.connect(uri) as websocket:
         message = await websocket.recv()
-		print(f"received message: {message}")
+        print(f"received message: {message}")
         msg = json.loads(message)
         cmd = msg.get('cmd', '')
         
         if cmd == 'shine':
-	        color = msg.get('color', '#FFFFFF')
-    	    print(f" > shining with color {color}")
+            color = msg.get('color', '#FFFFFF')
+            print(f" > shining with color {color}")
             await asyncio.create_task(strip.stripColor(color))
-			
-		elif cmd == 'rainbow':
+            
+        elif cmd == 'rainbow':
             await asyncio.create_task(strip.rainbow())
 
-		elif cmd == 'off':
+        elif cmd == 'off':
             await asyncio.create_task(strip.stripColor('#000000'))
 
 # Main program logic follows:
 if __name__ == '__main__':
-	# must be run as root
-	if not os.geteuid() == 0:
-	    sys.exit('This script must be run as root in order to control the LED.')
+    # must be run as root
+    if not os.geteuid() == 0:
+        sys.exit('This script must be run as root in order to control the LED.')
 
-	# open the web socket and listen for commands
-	asyncio.get_event_loop().run_until_complete(listen())
+    # open the web socket and listen for commands
+    asyncio.get_event_loop().run_until_complete(listen())

065007483f65dbb89746655c3f474be08ae1b301
Justin Weisz <jweisz@us.ibm.com>
2020-01-30 14:45:58 -0500

    hm, it didn't like the backticks

--- boot/install.sh
+++ boot/install.sh
@@ -3,6 +3,6 @@ sudo cp tjlamp.service /etc/systemd/system
 sudo chmod 644 /etc/systemd/system/tjlamp.service
 systemctl daemon-reload
 systemctl enable tjlamp
-echo "service installed! execute `systemctl start tjlamp` to run the service"
+echo "service installed! execute 'systemctl start tjlamp' to run the service"
 echo "and make sure tjlamp-client is installed at /home/pi/Desktop/tjlamp-client/"
 echo "(or update /etc/systemd/system/tjlamp.service with the right path)"

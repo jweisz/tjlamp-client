5240b85f8e8fc24a16f16189b8425c8c9c3683d1
Justin Weisz <jweisz@us.ibm.com>
2020-02-10 09:20:04 -0500

    more servo testing

--- test-servo.py
+++ test-servo.py
@@ -4,6 +4,8 @@ import argparse
 import wiringpi
 import asyncio
 
+from Servo import Servo
+
 def armBack(pin):
     wiringpi.pwmWrite(pin, 60)
 
@@ -38,6 +40,15 @@ async def main(pin):
     except KeyboardInterrupt:
         armBack(pin)
 
+def main2(pin):
+    arm = Servo(pin, enable=True)
+    try:
+        arm.wave(2)
+        await asyncio.sleep(1)
+    except KeyboardInterrupt:
+        arm.armUp()
+
+
 # Main program logic follows:
 if __name__ == '__main__':
     # must be run as root
@@ -48,4 +59,4 @@ if __name__ == '__main__':
     parser.add_argument('--pin', type=int, help='BCM PIN number the servo is attached to', default=13)
     args = parser.parse_args()
 
-    asyncio.get_event_loop().run_until_complete(main(args.pin))
+    asyncio.get_event_loop().run_until_complete(main2(args.pin))
